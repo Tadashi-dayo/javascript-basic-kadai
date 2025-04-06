39896addb50ca2cd731fd2242d8807067cc17094
Tadashi-dayo <kino0545@yahoo.co.jp>
2025-04-06 15:13:07 +0900

    フォルダをアップ

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const pushButton = document.getElementById('btn');
+
+pushButton.addEventListener('click', () => {
+  // const text = document.getElementById('text').textContent;
+  // console.log(text);
+  document.getElementById('text').textContent = 'ボタンをクリックしました'
+});
